21c43e19e3e0debe82682a99d47cd6104a46bf54
Simone3838 <simone.paolini14@gmail.com>
2024-12-30 13:28:55 +0100

    Change nasm to i386-elf-gcc and change ld to i386-elf-ld

--- Source/Compile.sh
+++ Source/Compile.sh
@@ -1,7 +1,8 @@
 # Compile the OS
 nasm -f bin -o bootloader.bin bootloader.asm
-nasm -f elf -o kernel.o kernel.asm
-nasm -f elf -o GDT.o GDT.asm
+i386-elf-gcc -c kernel.asm -o kernel.o
+i386-elf-gcc -c GDT.asm -o GDT.o
+i386-elf-ld -Ttext 0x1000 -o kernel.bin kernel.o GDT.o --oformat binary
 ld -m elf_i386 -Ttext 0x1000 -o kernel.bin kernel.o GDT.o --oformat binary
 # Create the OS image
 dd if=bootloader.bin of=jiOS.img bs=512 count=1 conv=notrunc
