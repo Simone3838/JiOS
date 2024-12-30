0cd642fb0ddf8ae8ff0f275572a01a537d2a1d16
Simone3838 <simone.paolini14@gmail.com>
2024-12-30 11:13:59 +0100

    Update Compile.sh to include GDT

--- Source/Compile.sh
+++ Source/Compile.sh
@@ -1,5 +1,7 @@
 # Compile the OS
 nasm -f bin -o bootloader.bin bootloader.asm
-nasm -f bin -o kernel.bin kernel.asm
+nasm -f elf -o kernel.o kernel.asm
+nasm -f elf -o GDT.o GDT.asm
+ld -m elf_i386 -Ttext 0x1000 -o kernel.bin kernel.o GDT.o --oformat binary
 # Create the OS image
 cat bootloader.bin kernel.bin > jiOS.img
